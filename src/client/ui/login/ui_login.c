88bb0d9e9c0c5fe25cb4d8082b4089b92a67f059
Francesco <francesco8995@gmail.com>
2025-07-22 02:28:02 +0200

    started google oauth implementation

--- src/client/ui/login/ui_login.c
+++ src/client/ui/login/ui_login.c
@@ -1,12 +1,42 @@
 #include "gio/gio.h"
 #include "glib-object.h"
 #include <gtk/gtk.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include "../ui.h"
+#include "../../../shared/shared.h"
 #include "gtk/gtkshortcut.h"
 
 GtkBuilder *builder;
+
+void google_button_clicked() {
+
+	const char *url_format = "https://accounts.google.com/o/oauth2/v2/auth?access_type=offline&scope=openid%%20email%%20profile&include_granted_scopes=true&response_type=code&redirect_uri=https://localhost:8080/&client_id=%s";
+
+	char *url = malloc(strlen(url_format) + strlen(GOOGLE_CLIENT_ID) + 1);
+
+	snprintf(url, strlen(url_format) + strlen(GOOGLE_CLIENT_ID), url_format, GOOGLE_CLIENT_ID);
+
+	char *command = malloc(strlen(url) + strlen("xdg-open '%s'"));
+
+	snprintf(command, strlen(url) + strlen("xdg-open '%s'") + 1, "xdg-open '%s'", url);
+
+	printf("COMMAND: [%s]\n", command);
+
+	system(command);
+}
+
+void setup_login_mode() {
+
+	GtkButton *google_login = GTK_BUTTON(gtk_builder_get_object(builder, "login_mode_google"));
+	g_signal_connect(google_login, "clicked", G_CALLBACK(google_button_clicked), NULL);
+}
+
+void setup_register_mode() {
+
+}
 
 void login_mode_btn_clicked(GtkButton *button, gpointer arg) {
 	char *data = (char *) arg;
@@ -17,6 +47,7 @@ void login_mode_btn_clicked(GtkButton *button, gpointer arg) {
 
 	if (strcmp(data, "login") == 0) {
 		gtk_stack_set_visible_child_name(stack, "login_page");
+		setup_login_mode();
 	} else {
 		gtk_stack_set_visible_child_name(stack, "register_page");
 	}
@@ -29,6 +60,8 @@ int display_login() {
 	GtkWidget *content = GTK_WIDGET(gtk_builder_get_object(builder, "background"));
 
 	gtk_window_set_child(get_app_window(), content);
+
+	setup_login_mode();
 
 	GtkButton *login_mode_btn = GTK_BUTTON(gtk_builder_get_object(builder, "login_mode_btn"));
 	GtkButton *register_mode_btn = GTK_BUTTON(gtk_builder_get_object(builder, "register_mode_btn"));
--- src/shared/shared.h
+++ src/shared/shared.h
@@ -6,7 +6,7 @@ SHARED
 
 #define PORT 35535
 #define BACKLOG 100
-#define GOOGLE_CLIENT_ID 1007096808449-g570r0cbkktk1cpnsf915vk1lviujamq.apps.googleusercontent.com
+#define GOOGLE_CLIENT_ID "1007096808449-g570r0cbkktk1cpnsf915vk1lviujamq.apps.googleusercontent.com"
 
 #endif 
 
